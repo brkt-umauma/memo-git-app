8fca3f113398bcb5aeedd544fb1d0c7e615f1771
brkt_umauma <brkt.tnkt@gmail.com>
2026-05-17 00:19:28 +0900

    feat: create basic editor window

--- desktopApp/src/main/kotlin/org/example/demo/main.kt
+++ desktopApp/src/main/kotlin/org/example/demo/main.kt
@@ -1,13 +1,32 @@
 package org.example.demo
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "demo",
+        title = "Memo App",
     ) {
-        App()
+
+        var text by remember {
+            mutableStateOf("")
+        }
+
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier.fillMaxSize()
+        )
+
     }
 }
