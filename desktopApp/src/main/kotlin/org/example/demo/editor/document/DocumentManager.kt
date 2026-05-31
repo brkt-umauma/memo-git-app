9564fef2f046406579881fa7972d078abac1242b
brkt_umauma <brkt.tnkt@gmail.com>
2026-06-01 01:11:13 +0900

    implement create within same tab

--- desktopApp/src/main/kotlin/org/example/demo/editor/document/DocumentManager.kt
+++ desktopApp/src/main/kotlin/org/example/demo/editor/document/DocumentManager.kt
@@ -1,5 +1,7 @@
 package org.example.demo.editor.document
 
+import javax.xml.crypto.dsig.Transform
+
 class DocumentManager {
     private val documents = mutableListOf<Document>()
 
@@ -18,5 +20,23 @@ class DocumentManager {
         activeIndex = documents.lastIndex
     }
 
-    // 
+    //ドキュメントを開く
+    fun openDocument(text: String, path: String?) {
+        documents.add(Document(text= text, currentFilePath = path,savedText= text))
+        activeIndex = documents.lastIndex
+    }
+
+    // アクティブなドキュメントの書き換え
+    fun updateActiveDocument(transform: (Document) -> Document) {
+        val current = activeDocument ?: return
+
+        documents[activeIndex] = transform(current)
+    }
+
+    // 保存状態の更新
+    fun markSaved(path: String?) {
+        updateActiveDocument { doc ->
+            doc.copy(currentFilePath = path, savedText = doc.text)
+        }
+    }
 }
